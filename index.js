eebea0479e1ef59baf10c3e949485298d4c451de
Ruchit <official.ruchit@gmail.com>
2024-06-27 19:15:40 +0530

    Implemented node app with login and registraition funtionality

--- index.js
+++ index.js
@@ -14,8 +14,5 @@ app.use(bodyParser.json());
 // Use the routes
 app.use("/api/auth", require("./routes/authRoute"));
 
-// Home route
-app.get("/", (req, res) => res.send("Hello World!"));
-
 // Start server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
