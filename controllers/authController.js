43c635b7fa3cb6dd986b554e56ba1035884b9dd9
Ruchit <official.ruchit@gmail.com>
2024-06-27 19:17:21 +0530

    Implemented node app with login and registraition funtionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,23 +47,23 @@ exports.login = async (req, res) => {
 };
 
 exports.updatePassword = async (req, res) => {
-    const { currentPassword, newPassword } = req.body;
-  
-    try {
-      const user = await User.findById(req.user.id);
-  console.log(user.name)
-      const isMatch = await bcrypt.compare(currentPassword, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ msg: 'Current password is incorrect' });
-      }
-  
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(newPassword, salt);
-      await user.save();
-  
-      res.json({ msg: 'Password updated successfully' });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
+  const { currentPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findById(req.user.id);
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Current password is incorrect" });
     }
-  };
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+    await user.save();
+
+    res.json({ msg: "Password updated successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
